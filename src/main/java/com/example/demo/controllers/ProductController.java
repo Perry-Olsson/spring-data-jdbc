f31850794aae9e6174a44a04f039bcd0883ce1f9
Perry <perry.olsson@smartsheet.com>
2022-05-10 18:32:12 -0700

    response entity on product controller

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.entities.Product;
 import com.example.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class ProductController {
+public class ProductController extends BaseController {
     ProductRepository productRepository;
 
     @Autowired
@@ -19,7 +20,8 @@ public class ProductController {
     }
 
     @GetMapping("/product/{name}")
-    List<Product> findAllByName(@PathVariable String name) {
-        return productRepository.findByName(name);
+    ResponseEntity<List<Product>> findAllByName(@PathVariable String name) {
+        List<Product> products = productRepository.findByName(name);
+        return httpOk().body(products);
     }
 }
